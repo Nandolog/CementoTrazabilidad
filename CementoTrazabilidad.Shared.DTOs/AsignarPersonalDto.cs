a97fc4b631d2bdb707c8f811f9b88b8779ba73e3
Nandolog <ferevers098@gmail.com>
2026-07-21 16:42:47 -0300

    Se borraron archivos y carpetas duplicadas

--- CementoTrazabilidad.Shared.DTOs/AsignarPersonalDto.cs
+++
@@ -1,8 +0,0 @@
-namespace CementoTrazabilidad.Shared.DTOs
-{
-    public class AsignarPersonalDto
-    {
-        public int PersonalId { get; set; }
-        public string? RolTurno { get; set; }
-    }
-}
